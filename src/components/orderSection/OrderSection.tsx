51b7fe2d7f9288579611ba94bf9f7ea0b01c6247
Joonatan Nguyen <minhnguyen181199@gmail.com>
2020-03-24 12:58:45 +0200

    Deactivate order list section

--- src/components/orderSection/OrderSection.tsx
+++ src/components/orderSection/OrderSection.tsx
@@ -8,6 +8,9 @@ const OrderSection = () => {
     <div className="order-section">
       <h3>Orders</h3>
       <table>
+        <div className="order-section__comming-soon">
+          <p>TO BE CONTINUE</p>
+        </div>
         <tbody>
           <tr>
             <th>200 grams</th>
